737d110a74ba8f4601598f7b062cc55d57a5b48c
Michael Penn <mwpenn94@gmail.com>
2020-12-11 03:59:38 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -97,6 +97,20 @@ def over?(board)
   end
 end
 
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
+
+def play(board)
+  counter = 0
+  until counter == 9
+  turn(board)
+  counter += 1
+  end
+end
+
 def play(board)
   until over?(board)
     turn(board) 
